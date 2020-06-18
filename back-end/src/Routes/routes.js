0b14031e91634a126a591b3dd6c88885237bcd14
André Rebonato <andrerebonato@users.noreply.github.com>
2020-06-18 11:42:29 -0300

    Adicionado novas rotas

--- back-end/src/Routes/routes.js
+++ back-end/src/Routes/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import UserController from '../Controllers/UserController';
+import { TwitterCrawlerController } from '../Controllers/TwitterCrawlerController';
+import { AuthController } from '../Controllers/AuthController';
 import { configs } from '../Configs/configs';
 
 const routes = Router();
 
 routes.get(configs.routes.user.getAll, UserController.getAll);
+routes.post(configs.routes.twitter.sendTweet, TwitterCrawlerController.tweet);
+routes.post(configs.routes.auth.login, AuthController.generateToken);
 
 export default routes;
